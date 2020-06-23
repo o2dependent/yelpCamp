a87d679f39f8a73e221543e8676bb6cbcf50d809
Ethan Olsen <131eolse@gmail.com>
2020-06-23 19:36:06 +0000

    add port listening to env ip and port

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT,process.env.IP, () => {
 	console.log("Yelp Camp online");
 });
