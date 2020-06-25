dc0cbf5948eb683b612b2a35dcfc7b6712513e9f
Ethan Olsen <131eolse@gmail.com>
2020-06-25 22:24:10 +0000

    app.js env update

--- app.js
+++ app.js
@@ -56,6 +56,6 @@ app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
 
-app.listen(process.env.PORT || 3000,process.env.IP, () => {
+app.listen(process.env.PORT, process.env.IP, () => {
 	console.log("Yelp Camp online");
 });
